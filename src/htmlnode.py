754a712e56193aab908e9f796f542859ff17c94c
narugykivi <git@gyuran.hu>
2026-05-09 17:46:36 +0200

    Creating ParentNode for representing a nested HTML tags with children, updating unittest

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,18 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("HTMLParent missing a tag attribute")
+        if self.children is None:
+            raise ValueError("HTMLParent missing a children attribute")
+        html = ""
+        for child in self.children:
+            html += child.to_html()
+        return f"<{self.tag}>{html}</{self.tag}>"
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -50,6 +50,18 @@ class TestHTMLNode(unittest.TestCase):
             repr(node)
         )
 
+    def test_to_html(self):
+        node = HTMLNode(
+            "link",
+            "Boot.dev",
+            None,
+            { "href": "https://www.google.com", "target": "_blank",}
+        )
+        with self.assertRaises(NotImplementedError) as cm:
+            node.to_html()
+        self.assertTrue("The 'to_html()' method is not implemented yet." in str(cm.exception))
+
+class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
@@ -62,7 +74,48 @@ class TestHTMLNode(unittest.TestCase):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
 
+    def test_leaf_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError) as cm:
+            node.to_html()
+        self.assertTrue("All leaf nodes must have a value" in str(cm.exception))
 
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_whitout_children(self):
+        parent_node = ParentNode("div", None)
+        with self.assertRaises(ValueError) as cm:
+            parent_node.to_html()
+        self.assertTrue("HTMLParent missing a children attribute" in str(cm.exception))
+
+    def test_to_html_without_tag(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode(None, [child_node])
+        with self.assertRaises(Exception) as cm:
+            parent_node.to_html()
+        self.assertTrue("HTMLParent missing a tag attribute" in str(cm.exception))
+
+    def test_to_html_with_props(self):
+        grandchild_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            '<div><span><a href="https://www.google.com">Click me!</a></span></div>',
+        )
 
 
 if __name__ == "__main__":
