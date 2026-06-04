d5f0de066bf438c0010bd62e1d31ed2f73e4d449
narugykivi <git@gyuran.hu>
2026-06-04 16:15:50 +0200

    Created a function to generate a page from markdown via a template to html

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,6 @@
-from blocks import markdown_to_blocks, block_to_block_type, BlockType
+import os
+
+from blocks import markdown_to_blocks, block_to_block_type, markdown_to_html_node, BlockType
 
 def extract_title(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -12,4 +14,19 @@ def extract_title(markdown):
     raise Exception("There is no 'h1' header in the markdown text")
 
 def generate_page(from_path, template_path, dest_path):
-    pass
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    html_template = None
+    raw_markdown = None
+    with open(from_path, 'r', encoding="utf-8") as f:
+        raw_markdown = f.read()
+    with open(template_path, 'r', encoding="utf-8") as f:
+        html_template = f.read()
+    content = markdown_to_html_node(raw_markdown).to_html()
+    title = extract_title(raw_markdown)
+    html_template = html_template.replace("{{ Title }}", title)
+    html_template = html_template.replace("{{ Content }}", content)
+    print(content, "\n", title, "\n", html_template)
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(dest_path)
+    with open(dest_path, "w") as f:
+        f.write(html_template)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
 from clean_copy import copy_all
+from generate_page import generate_page
 
 def main():
     copy_all()
+    generate_page("content/index.md", "template.html", "public/index.html")
     
 main()
